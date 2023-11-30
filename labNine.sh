d092d4aa9ca74cafcad8090c658eb9a02a81d750
LemonIc3 <nick19667600@gmail.com>
2023-11-30 18:14:34 -0500

    Finished comments

--- labNine.sh
+++ labNine.sh
@@ -10,9 +10,8 @@
 #This function makes a new file called Copy_file, which is the file that has the madlib stored to edit.
 
 function copyDoc(){
-    local source_file=$1 # This is the file that will be copied
-
-    #Set variable to the current working directory
+    #This function creates a copy of the original
+    local source_file=$1
     local destination_directory="$PWD"
 
     local base_name="copy_file.txt"
@@ -36,8 +35,8 @@ function madLib(){
     
     # Count the exact occurrences of the placeholder word
     local line_count=$(awk -v word="$wordToRplce" '{ count += gsub(word, "") } END { print count }' "$madFile")
-    
 
+    
     cat "$madFile"
     echo ""
     echo ""
@@ -63,11 +62,13 @@ function madLib(){
 }
 
 function replaceAll(){
+    #Just the set of local variables, each $(number) is the order in which they will be called.
     local word_to_replace=$1
     local replacement_word=$2
     local cpFile=$3
 
     # Replace all occurrences of a particular word in a file
+    #This line of code starts with awk which is used to read from the copy file and creates a temp file which then overrites the copyfile.
     awk -v old="$word_to_replace" -v new="$replacement_word" '{ gsub(old, new) } 1' "$cpFile" > tmpfile.txt && mv tmpfile.txt "$cpFile"
     
 }
@@ -95,6 +96,9 @@ function replaceFirst() {
 }
 
 function main() {
+    #This function essential holds the menu which you can select optons from.
+
+     
     local user=$(whoami)
     local pathLink=$1
 
@@ -103,15 +107,12 @@ function main() {
     madLib "$newFile"
 
     local sentinel=true
-
-    
+#This is the main menu loop that will only exit upon entering (q) other wise we have 3 function to choose from.
     while [ "$sentinel" = true ]; do
-
-        #After the madlib is finished, this prompt will appear and ask the user what they want to do next. 
-        #Input the letter in the parenthesis to make your choice.
         echo "$user, do you want to (R)eplace all, Replace (F)irst occurrence, new (M)adLib, or (Q)uit."
         read -r userCommand
 
+        #If (R) is entered after the creation of your madLib you can replace all of the instances of a word you used.
         if [ "$userCommand" = "R" ] || [ "$userCommand" = "r" ]; then
             echo "Enter the word to replace:"
             read -r word_to_replace
@@ -119,6 +120,7 @@ function main() {
             read -r replacement_word
             replaceAll "$word_to_replace" "$replacement_word" "$newFile"
             echo "Modified madlib: $(cat "$newFile")"
+        #Entering (F) will allow you to replace the first line of the madlib
         elif [ "$userCommand" = "F" ] || [ "$userCommand" = "f" ]; then
             echo "Enter the word to replace:"
             read -r word_to_replace
@@ -126,11 +128,14 @@ function main() {
             read -r replacement_word
             replaceFirst "$word_to_replace" "$replacement_word" "$newFile"
             echo "Modified madlib: $(cat "$newFile")"
+        #Allows you to create a new madlib upon entering (M).
         elif [ "$userCommand" = "M" ] || [ "$userCommand" = "m" ]; then
             local copyFile=$(copyDoc "$pathLink")
             madLib "$copyFile"
+        #Entering (q) will quit out the program.
         elif [ "$userCommand" = "Q" ] || [ "$userCommand" = "q" ]; then
             sentinel=false
+        #This else statement is for input validation to check if you entered one of the above options.
         else
             echo "Please retry, $user, you entered invalid input."
         fi
